6cb46e5c8102199a7bafc3a1b5956b7dc7c7896f
yohannlehazif-wq <yohannlehazif@gmail.com>
2026-05-07 12:23:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,8 +11,13 @@ const urlsToCache = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
